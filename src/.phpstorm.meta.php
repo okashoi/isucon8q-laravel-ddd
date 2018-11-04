5b4629b47187e9cef3190af857743020fa481c2f
Okada Shohei <okashoicircus0@gmail.com>
2018-11-05 04:29:18 +0900

    Install doctrine/dbal

--- src/.phpstorm.meta.php
+++ src/.phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-11-05 04:22:24.
+    * Generated on 2018-11-05 04:27:50.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
