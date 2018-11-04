8194bedb526198e3c4ad435b06b5382d36e89823
Okada Shohei <okashoicircus0@gmail.com>
2018-11-05 05:25:45 +0900

    Add PHPDoc to Core\Exceptions\Handler::render()

--- src/core/Exceptions/Handler.php
+++ src/core/Exceptions/Handler.php
@@ -46,6 +46,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        /** @var \Illuminate\Http\Response $response */
+        $response = parent::render($request, $exception);
+
+        return $response;
     }
 }
